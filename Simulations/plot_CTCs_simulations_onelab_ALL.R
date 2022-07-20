5bbbaa733854b73d360287c1be1928152036612d
Olivier Pasche <Olivier.Pasche@unige.ch>
2022-07-20 20:21:38 +0200

    black colour segments for population values

--- Simulations/plot_CTCs_simulations_onelab_ALL.R
+++ Simulations/plot_CTCs_simulations_onelab_ALL.R
@@ -14,25 +14,25 @@ source("../R_functions/CTC_contribution_functions_OCP.R")
 
 save_path <- "Results/CTCs_simulations/q90_1M_2/"#"Results/CTCs_simulations/" or NULL
 sims_path <- "CTCs_simulations/q90_1M/"
-check_directory(dirname(save_path), recursive=TRUE)
+check_directory(save_path, recursive=TRUE)
 
 #files_list <- list.files(path="CTCs_simulations/q90_1M/", pattern="CTCs_*_1M2k1000.RData", full.names=FALSE)
 general_params_code <- "_q90_1M2k1000"#"_q90_100k2k1000""_q90_10k2k1000""_1M2k1000"
-distr_codes <- c("ln",
-                 "ln_H0p5",
+distr_codes <- c("ln_H0p5",
                  "ln_H1p5",
                  "ln_H2",
-                 "Pa1-2",
-                 "Pa1-2_H1-1",
-                 "Pa2_H1p5",
-                 "Pa2_H1p2",
+                 "ln",
                  "Pa1-2_H1-4",
+                 # "Pa1-2_H1-1",
+                 "Pa1-2",
+                 # "Pa2_H1p2",
+                 "Pa2_H1p5",
                  "Pa2p3_H1p8",
                  "Pa3_H1p5",
-                 "t4",
                  "t4_H2",
                  "t4_H3",
-                 "t4_H6")
+                 "t4_H6",
+                 "t4")
 codes_list <- paste0(distr_codes, general_params_code)
 
 #Plots widths and heights for the 2 types of plots
@@ -99,32 +99,36 @@ for(sims_code in codes_list){
     annotate_figure(left=text_grob("Density", rot = 90), bottom="Non-parametric CTC difference")
   #plot(pdif_ctc)
   Sys.sleep(0.5)
-  ggsave(paste(sims_code, "_dif_npctc.png", sep=""), plot=pdif_ctc, device="png", path=save_path, width=Wdif, height=Hdif, units="mm", dpi=300)
+  ggsave(paste(sims_code, "_dif_npctc.pdf", sep=""), plot=pdif_ctc, device="pdf", path=save_path, width=Wdif, height=Hdif, units="mm", dpi=300)
   Sys.sleep(0.5)
   
   plot_xlimsi <- c(min(c(ctc12.i,ctc21.i,ctc12.ih,ctc21.ih, 0.75),na.rm=TRUE), 1)
   plot_xlimsc <- c(min(c(ctc12.c,ctc21.c,ctc12.ch,ctc21.ch, 0.75),na.rm=TRUE), 1)
   pci <- ggplot(data.frame(Gamma12=ctc12.i, Gamma21=ctc21.i), aes(x=x) ) +
+    geom_segment(x=0.5, xend=0.5, y=0, yend=Inf, col="black", size=1) + geom_segment(x=0.5, xend=0.5, y=0, yend=-Inf, col="black", size=1) +
     geom_histogram(aes(x=Gamma12, y=..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v12) +
     geom_histogram(aes(x=Gamma21, y=-..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v21) + labs(title="Independent variables", x=NULL, y=NULL) +
-    expand_limits(x=plot_xlimsi) + geom_segment(x=0.5, xend=0.5, y=0, yend=Inf, col=col_v12, size=1) + geom_segment(x=0.5, xend=0.5, y=0, yend=-Inf, col=col_v21, size=1)
+    expand_limits(x=plot_xlimsi)
   pcih <- ggplot(data.frame(Gamma12=ctc12.ih, Gamma21=ctc21.ih), aes(x=x) ) +
+    geom_segment(x=0.75, xend=0.75, y=0, yend=Inf, col="black", size=1) + geom_segment(x=0.75, xend=0.75, y=0, yend=-Inf, col="black", size=1) +
     geom_histogram(aes(x=Gamma12, y=..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v12) +
     geom_histogram(aes(x=Gamma21, y=-..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v21) + labs(title="Common hidden confounder", x=NULL, y=NULL) +
-    expand_limits(x=plot_xlimsi) + geom_segment(x=0.75, xend=0.75, y=0, yend=Inf, col=col_v12, size=1) + geom_segment(x=0.75, xend=0.75, y=0, yend=-Inf, col=col_v21, size=1)
+    expand_limits(x=plot_xlimsi)
   pcc <- ggplot(data.frame(Gamma12=ctc12.c, Gamma21=ctc21.c), aes(x=x) ) +
+    geom_segment(x=1, xend=1, y=0, yend=Inf, col="black", size=1) + geom_segment(x=0.75, xend=0.75, y=0, yend=-Inf, col="black", size=1) +
     geom_histogram(aes(x=Gamma12, y=..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v12) +
     geom_histogram(aes(x=Gamma21, y=-..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v21) + labs(title="X1 -> X2", x=NULL, y=NULL) +
-    expand_limits(x=plot_xlimsc) + geom_segment(x=1, xend=1, y=0, yend=Inf, col=col_v12, size=1) + geom_segment(x=0.75, xend=0.75, y=0, yend=-Inf, col=col_v21, size=1)
+    expand_limits(x=plot_xlimsc)
   pcch <- ggplot(data.frame(Gamma12=ctc12.ch, Gamma21=ctc21.ch), aes(x=x) ) +
+    geom_segment(x=1, xend=1, y=0, yend=Inf, col="black", size=1) + geom_segment(x=7/8, xend=7/8, y=0, yend=-Inf, col="black", size=1) +
     geom_histogram(aes(x=Gamma12, y=..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v12) +
     geom_histogram(aes(x=Gamma21, y=-..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v21) + labs(title="X1 -> X2 with confounder", x=NULL, y=NULL) +
-    expand_limits(x=plot_xlimsc) + geom_segment(x=1, xend=1, y=0, yend=Inf, col=col_v12, size=1) + geom_segment(x=7/8, xend=7/8, y=0, yend=-Inf, col=col_v21, size=1)
+    expand_limits(x=plot_xlimsc)
   pvals_ctc <- ggarrange(pci,pcih,pcc,pcch, labels=c("A", "B", "C", "D"), ncol=2, nrow=2) %>%
     annotate_figure(left=text_grob("Density", rot = 90), bottom="Non-parametric causal tail coefficients")
   #plot(pvals_ctc)
   Sys.sleep(0.5)
-  ggsave(paste(sims_code, "_vals_npctc.png", sep=""), plot=pvals_ctc, device="png", path=save_path, width=Wvals, height=Hvals, units="mm", dpi=300)
+  ggsave(paste(sims_code, "_vals_npctc.pdf", sep=""), plot=pvals_ctc, device="pdf", path=save_path, width=Wvals, height=Hvals, units="mm", dpi=300)
   Sys.sleep(0.5)
   
   
@@ -154,32 +158,36 @@ for(sims_code in codes_list){
     annotate_figure(left=text_grob("Density", rot = 90), bottom="GPD CTC difference")
   #plot(pdif_gpdctc)
   Sys.sleep(0.5)
-  ggsave(paste(sims_code, "_dif_gpdctc.png", sep=""), plot=pdif_gpdctc, device="png", path=save_path, width=Wdif, height=Hdif, units="mm", dpi=300)
+  ggsave(paste(sims_code, "_dif_gpdctc.pdf", sep=""), plot=pdif_gpdctc, device="pdf", path=save_path, width=Wdif, height=Hdif, units="mm", dpi=300)
   Sys.sleep(0.5)
   
   plot_xlimsi <- c(min(c(gpdctc12.i,gpdctc21.i,gpdctc12.ih,gpdctc21.ih, 0.75),na.rm=TRUE), 1)
   plot_xlimsc <- c(min(c(gpdctc12.c,gpdctc21.c,gpdctc12.ch,gpdctc21.ch, 0.75),na.rm=TRUE), 1)
   pci <- ggplot(data.frame(Gamma12=gpdctc12.i, Gamma21=gpdctc21.i), aes(x=x) ) +
+    geom_segment(x=0.5, xend=0.5, y=0, yend=Inf, col="black", size=1) + geom_segment(x=0.5, xend=0.5, y=0, yend=-Inf, col="black", size=1) +
     geom_histogram(aes(x=Gamma12, y=..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v12) +
     geom_histogram(aes(x=Gamma21, y=-..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v21) + labs(title="Independent variables", x=NULL, y=NULL) +
-    expand_limits(x=plot_xlimsi) + geom_segment(x=0.5, xend=0.5, y=0, yend=Inf, col=col_v12, size=1) + geom_segment(x=0.5, xend=0.5, y=0, yend=-Inf, col=col_v21, size=1)
+    expand_limits(x=plot_xlimsi)
   pcih <- ggplot(data.frame(Gamma12=gpdctc12.ih, Gamma21=gpdctc21.ih), aes(x=x) ) +
+    geom_segment(x=0.75, xend=0.75, y=0, yend=Inf, col="black", size=1) + geom_segment(x=0.75, xend=0.75, y=0, yend=-Inf, col="black", size=1) +
     geom_histogram(aes(x=Gamma12, y=..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v12) +
     geom_histogram(aes(x=Gamma21, y=-..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v21) + labs(title="Common hidden confounder", x=NULL, y=NULL) +
-    expand_limits(x=plot_xlimsi) + geom_segment(x=0.75, xend=0.75, y=0, yend=Inf, col=col_v12, size=1) + geom_segment(x=0.75, xend=0.75, y=0, yend=-Inf, col=col_v21, size=1)
+    expand_limits(x=plot_xlimsi)
   pcc <- ggplot(data.frame(Gamma12=gpdctc12.c, Gamma21=gpdctc21.c), aes(x=x) ) +
+    geom_segment(x=1, xend=1, y=0, yend=Inf, col="black", size=1) + geom_segment(x=0.75, xend=0.75, y=0, yend=-Inf, col="black", size=1) +
     geom_histogram(aes(x=Gamma12, y=..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v12) +
     geom_histogram(aes(x=Gamma21, y=-..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v21) + labs(title="X1 -> X2", x=NULL, y=NULL) +
-    expand_limits(x=plot_xlimsc) + geom_segment(x=1, xend=1, y=0, yend=Inf, col=col_v12, size=1) + geom_segment(x=0.75, xend=0.75, y=0, yend=-Inf, col=col_v21, size=1)
+    expand_limits(x=plot_xlimsc)
   pcch <- ggplot(data.frame(Gamma12=gpdctc12.ch, Gamma21=gpdctc21.ch), aes(x=x) ) +
+    geom_segment(x=1, xend=1, y=0, yend=Inf, col="black", size=1) + geom_segment(x=7/8, xend=7/8, y=0, yend=-Inf, col="black", size=1) +
     geom_histogram(aes(x=Gamma12, y=..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v12) +
     geom_histogram(aes(x=Gamma21, y=-..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v21) + labs(title="X1 -> X2 with confounder", x=NULL, y=NULL) +
-    expand_limits(x=plot_xlimsc) + geom_segment(x=1, xend=1, y=0, yend=Inf, col=col_v12, size=1) + geom_segment(x=7/8, xend=7/8, y=0, yend=-Inf, col=col_v21, size=1)
+    expand_limits(x=plot_xlimsc)
   pvals_gpdctc <- ggarrange(pci,pcih,pcc,pcch, labels=c("A", "B", "C", "D"), ncol=2, nrow=2) %>%
     annotate_figure(left=text_grob("Density", rot = 90), bottom="GPD causal tail coefficients")
   #plot(pvals_gpdctc)
   Sys.sleep(0.5)
-  ggsave(paste(sims_code, "_vals_gpdctc.png", sep=""), plot=pvals_gpdctc, device="png", path=save_path, width=Wvals, height=Hvals, units="mm", dpi=300)
+  ggsave(paste(sims_code, "_vals_gpdctc.pdf", sep=""), plot=pvals_gpdctc, device="pdf", path=save_path, width=Wvals, height=Hvals, units="mm", dpi=300)
   Sys.sleep(0.5)
   
   
@@ -209,35 +217,38 @@ for(sims_code in codes_list){
     annotate_figure(left=text_grob("Density", rot = 90), bottom="Post-fit corrected LGPD CTC difference")
   #plot(pdif_pfclgpdctc)
   Sys.sleep(0.5)
-  ggsave(paste(sims_code, "_dif_pfcorr_lgpdctc.png", sep=""), plot=pdif_pfclgpdctc, device="png", path=save_path, width=Wdif, height=Hdif, units="mm", dpi=300)
+  ggsave(paste(sims_code, "_dif_pfcorr_lgpdctc.pdf", sep=""), plot=pdif_pfclgpdctc, device="pdf", path=save_path, width=Wdif, height=Hdif, units="mm", dpi=300)
   Sys.sleep(0.5)
-  
   
   plot_xlimsi <- c(min(c(pfcorr_lgpdctc12.i,pfcorr_lgpdctc21.i,pfcorr_lgpdctc12.ih,pfcorr_lgpdctc21.ih, 0.75),na.rm=TRUE), 1)
   plot_xlimsc <- c(min(c(pfcorr_lgpdctc12.c,pfcorr_lgpdctc21.c,pfcorr_lgpdctc12.ch,pfcorr_lgpdctc21.ch, 0.75),na.rm=TRUE), 1)
   pci <- ggplot(data.frame(Gamma12=pfcorr_lgpdctc12.i, Gamma21=pfcorr_lgpdctc21.i), aes(x=x) ) +
+    geom_segment(x=0.5, xend=0.5, y=0, yend=Inf, col="black", size=1) + geom_segment(x=0.5, xend=0.5, y=0, yend=-Inf, col="black", size=1) +
     geom_histogram(aes(x=Gamma12, y=..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v12) +
     geom_histogram(aes(x=Gamma21, y=-..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v21) + labs(title="Independent variables", x=NULL, y=NULL) +
-    expand_limits(x=plot_xlimsi) + geom_segment(x=0.5, xend=0.5, y=0, yend=Inf, col=col_v12, size=1) + geom_segment(x=0.5, xend=0.5, y=0, yend=-Inf, col=col_v21, size=1)
+    expand_limits(x=plot_xlimsi)
   pcih <- ggplot(data.frame(Gamma12=pfcorr_lgpdctc12.ih, Gamma21=pfcorr_lgpdctc21.ih), aes(x=x) ) +
+    geom_segment(x=0.75, xend=0.75, y=0, yend=Inf, col="black", size=1) + geom_segment(x=0.75, xend=0.75, y=0, yend=-Inf, col="black", size=1) +
     geom_histogram(aes(x=Gamma12, y=..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v12) +
     geom_histogram(aes(x=Gamma21, y=-..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v21) + labs(title="Common hidden confounder", x=NULL, y=NULL) +
-    expand_limits(x=plot_xlimsi) + geom_segment(x=0.75, xend=0.75, y=0, yend=Inf, col=col_v12, size=1) + geom_segment(x=0.75, xend=0.75, y=0, yend=-Inf, col=col_v21, size=1)#+
+    expand_limits(x=plot_xlimsi)#+
   #geom_segment(x=0.5, xend=0.5, y=0, yend=Inf, col=col_v12, size=1) + geom_segment(x=0.5, xend=0.5, y=0, yend=-Inf, col=col_v21, size=1)
   pcc <- ggplot(data.frame(Gamma12=pfcorr_lgpdctc12.c, Gamma21=pfcorr_lgpdctc21.c), aes(x=x) ) +
+    geom_segment(x=1, xend=1, y=0, yend=Inf, col="black", size=1) + geom_segment(x=0.75, xend=0.75, y=0, yend=-Inf, col="black", size=1) +
     geom_histogram(aes(x=Gamma12, y=..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v12) +
     geom_histogram(aes(x=Gamma21, y=-..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v21) + labs(title="X1 -> X2", x=NULL, y=NULL) +
-    expand_limits(x=plot_xlimsc) + geom_segment(x=1, xend=1, y=0, yend=Inf, col=col_v12, size=1) + geom_segment(x=0.75, xend=0.75, y=0, yend=-Inf, col=col_v21, size=1)
+    expand_limits(x=plot_xlimsc)
   pcch <- ggplot(data.frame(Gamma12=pfcorr_lgpdctc12.ch, Gamma21=pfcorr_lgpdctc21.ch), aes(x=x) ) +
+    geom_segment(x=1, xend=1, y=0, yend=Inf, col="black", size=1) + geom_segment(x=7/8, xend=7/8, y=0, yend=-Inf, col="black", size=1) +
     geom_histogram(aes(x=Gamma12, y=..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v12) +
     geom_histogram(aes(x=Gamma21, y=-..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v21) + labs(title="X1 -> X2 with confounder", x=NULL, y=NULL) +
-    expand_limits(x=plot_xlimsc) + geom_segment(x=1, xend=1, y=0, yend=Inf, col=col_v12, size=1) + geom_segment(x=7/8, xend=7/8, y=0, yend=-Inf, col=col_v21, size=1)#+
+    expand_limits(x=plot_xlimsc)#+
   #geom_segment(x=0.75, xend=0.75, y=0, yend=-Inf, col=col_v21, size=1)
   pvals_pfclgpdctc <- ggarrange(pci,pcih,pcc,pcch, labels=c("A", "B", "C", "D"), ncol=2, nrow=2) %>%
     annotate_figure(left=text_grob("Density", rot = 90), bottom="Post-fit corrected LGPD CTCs")
   #plot(pvals_pfclgpdctc)
   Sys.sleep(0.5)
-  ggsave(paste(sims_code, "_vals_pfcorr_lgpdctc.png", sep=""), plot=pvals_pfclgpdctc, device="png", path=save_path, width=Wvals, height=Hvals, units="mm", dpi=300)
+  ggsave(paste(sims_code, "_vals_pfcorr_lgpdctc.pdf", sep=""), plot=pvals_pfclgpdctc, device="pdf", path=save_path, width=Wvals, height=Hvals, units="mm", dpi=300)
   Sys.sleep(0.5)
   
   
@@ -267,35 +278,38 @@ for(sims_code in codes_list){
     annotate_figure(left=text_grob("Density", rot = 90), bottom="Constrained fit LGPD CTC difference")
   #plot(pdif_constrlgpdctc)
   Sys.sleep(0.5)
-  ggsave(paste(sims_code, "_dif_constr_lgpdctc.png", sep=""), plot=pdif_constrlgpdctc, device="png", path=save_path, width=Wdif, height=Hdif, units="mm", dpi=300)
+  ggsave(paste(sims_code, "_dif_constr_lgpdctc.pdf", sep=""), plot=pdif_constrlgpdctc, device="pdf", path=save_path, width=Wdif, height=Hdif, units="mm", dpi=300)
   Sys.sleep(0.5)
-  
   
   plot_xlimsi <- c(min(c(constr_lgpdctc12.i,constr_lgpdctc21.i,constr_lgpdctc12.ih,constr_lgpdctc21.ih, 0.75),na.rm=TRUE), 1)
   plot_xlimsc <- c(min(c(constr_lgpdctc12.c,constr_lgpdctc21.c,constr_lgpdctc12.ch,constr_lgpdctc21.ch, 0.75),na.rm=TRUE), 1)
   pci <- ggplot(data.frame(Gamma12=constr_lgpdctc12.i, Gamma21=constr_lgpdctc21.i), aes(x=x) ) +
+    geom_segment(x=0.5, xend=0.5, y=0, yend=Inf, col="black", size=1) + geom_segment(x=0.5, xend=0.5, y=0, yend=-Inf, col="black", size=1) +
     geom_histogram(aes(x=Gamma12, y=..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v12) +
     geom_histogram(aes(x=Gamma21, y=-..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v21) + labs(title="Independent variables", x=NULL, y=NULL) +
-    expand_limits(x=plot_xlimsi) + geom_segment(x=0.5, xend=0.5, y=0, yend=Inf, col=col_v12, size=1) + geom_segment(x=0.5, xend=0.5, y=0, yend=-Inf, col=col_v21, size=1)
+    expand_limits(x=plot_xlimsi)
   pcih <- ggplot(data.frame(Gamma12=constr_lgpdctc12.ih, Gamma21=constr_lgpdctc21.ih), aes(x=x) ) +
+    geom_segment(x=0.75, xend=0.75, y=0, yend=Inf, col="black", size=1) + geom_segment(x=0.75, xend=0.75, y=0, yend=-Inf, col="black", size=1) +
     geom_histogram(aes(x=Gamma12, y=..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v12) +
     geom_histogram(aes(x=Gamma21, y=-..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v21) + labs(title="Common hidden confounder", x=NULL, y=NULL) +
-    expand_limits(x=plot_xlimsi) + geom_segment(x=0.75, xend=0.75, y=0, yend=Inf, col=col_v12, size=1) + geom_segment(x=0.75, xend=0.75, y=0, yend=-Inf, col=col_v21, size=1)#+
+    expand_limits(x=plot_xlimsi)#+
   #geom_segment(x=0.5, xend=0.5, y=0, yend=Inf, col=col_v12, size=1) + geom_segment(x=0.5, xend=0.5, y=0, yend=-Inf, col=col_v21, size=1)
   pcc <- ggplot(data.frame(Gamma12=constr_lgpdctc12.c, Gamma21=constr_lgpdctc21.c), aes(x=x) ) +
+    geom_segment(x=1, xend=1, y=0, yend=Inf, col="black", size=1) + geom_segment(x=0.75, xend=0.75, y=0, yend=-Inf, col="black", size=1) +
     geom_histogram(aes(x=Gamma12, y=..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v12) +
     geom_histogram(aes(x=Gamma21, y=-..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v21) + labs(title="X1 -> X2", x=NULL, y=NULL) +
-    expand_limits(x=plot_xlimsc) + geom_segment(x=1, xend=1, y=0, yend=Inf, col=col_v12, size=1) + geom_segment(x=0.75, xend=0.75, y=0, yend=-Inf, col=col_v21, size=1)
+    expand_limits(x=plot_xlimsc)
   pcch <- ggplot(data.frame(Gamma12=constr_lgpdctc12.ch, Gamma21=constr_lgpdctc21.ch), aes(x=x) ) +
+    geom_segment(x=1, xend=1, y=0, yend=Inf, col="black", size=1) + geom_segment(x=7/8, xend=7/8, y=0, yend=-Inf, col="black", size=1) +
     geom_histogram(aes(x=Gamma12, y=..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v12) +
     geom_histogram(aes(x=Gamma21, y=-..density..), binwidth=0.005,boundary=1,closed="right", fill=col_v21) + labs(title="X1 -> X2 with confounder", x=NULL, y=NULL) +
-    expand_limits(x=plot_xlimsc) + geom_segment(x=1, xend=1, y=0, yend=Inf, col=col_v12, size=1) + geom_segment(x=7/8, xend=7/8, y=0, yend=-Inf, col=col_v21, size=1)#+
+    expand_limits(x=plot_xlimsc)#+
   #geom_segment(x=0.75, xend=0.75, y=0, yend=-Inf, col=col_v21, size=1)
   pvals_constrlgpdctc <- ggarrange(pci,pcih,pcc,pcch, labels=c("A", "B", "C", "D"), ncol=2, nrow=2) %>%
     annotate_figure(left=text_grob("Density", rot = 90), bottom="Constrained fit LGPD CTCs")
   #plot(pvals_constrlgpdctc)
   Sys.sleep(0.5)
-  ggsave(paste(sims_code, "_vals_constr_lgpdctc.png", sep=""), plot=pvals_constrlgpdctc, device="png", path=save_path, width=Wvals, height=Hvals, units="mm", dpi=300)
+  ggsave(paste(sims_code, "_vals_constr_lgpdctc.pdf", sep=""), plot=pvals_constrlgpdctc, device="pdf", path=save_path, width=Wvals, height=Hvals, units="mm", dpi=300)
   Sys.sleep(0.5)
   
 }
